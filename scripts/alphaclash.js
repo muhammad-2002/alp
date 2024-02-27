4dfd653027801d5de750c1afb8aaa8e6d329fd96
muhammad-2002 <masumbilla10104@gmail.com>
2024-02-27 22:33:08 +0600

    music added

--- scripts/alphaclash.js
+++ scripts/alphaclash.js
@@ -1,4 +1,7 @@
+let audio = new Audio()
+let isPlayGame = false;
 function keyUpHandler (event){
+    if (isPlayGame ===false) return
     const expectedInput = event.key
     
     
@@ -11,14 +14,21 @@ function keyUpHandler (event){
 
     if(expectedInput === displayAlphabet){
         
+        audio.src='/audio/audio1.wav'
+        audio.play()
        let scoreId = paresIntser('scoreId')
         scoreId+=1;
        setElementById('scoreId', scoreId)
+       
     }else{
         removeBackgroudColorById(expectedInput)
         let lifeId = paresIntser('lifeId')
         lifeId-=1;
         setElementById('lifeId',lifeId)
+        audio.src='/audio/audio2.wav'
+        audio.play()
+
+
         if(lifeId === 0){
             
             let finalScore = getElementById('total')
@@ -26,9 +36,9 @@ function keyUpHandler (event){
             gameOver()
         }
     }
+   
     removeBackgroudColorById(displayAlphabet)
     continueGame()
-
 }
 document.addEventListener('keyup',keyUpHandler)
 function continueGame(){
@@ -36,17 +46,21 @@ function continueGame(){
     const currentAlphabet = getElementById('current-alphabet')
     currentAlphabet.innerText = randomAlphabets
     setBackgroudColorById(randomAlphabets)
+    console.log(randomAlphabets)
     
 }
 
 
 function play(){
-    removeBackgroudColorById('current-alphabet')
+    isPlayGame = true
     hideElementById('home-screen')
     hideElementById('final-score')
     showElementById('play-ground')
     setElementById('scoreId',0)
     setElementById('lifeId',5)
+    const praseAlphabet = document.getElementById('current-alphabet').innerText 
+    const displayAlphabet = praseAlphabet.toLowerCase()
+    removeBackgroudColorById(displayAlphabet)
     
     
     continueGame()
@@ -56,9 +70,8 @@ function play(){
 function gameOver(){
     hideElementById('play-ground')
     showElementById('final-score')
-    // const currentElements = getElementById('current-alphabet')
-    // // window.location.reload()
-    // console.log(currentElements)
+    isPlayGame= false
+    
      
 }
 
